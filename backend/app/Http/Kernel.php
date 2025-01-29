e811f1c3ec063833a346f519df38b5790dd095ac
S0han <kane.pereira18@gmail.com>
2025-01-29 01:35:55 -0500

    [create]: kernel.php and this will allow for route recognition

---
+++ backend/app/Http/Kernel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    protected $middlewareGroups = [
+        'api' => [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class, // Optional if using Sanctum
+            \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
+}
